905185a181cb45458963542098143b7336823454
Edgar Costa <edgarc@mit.edu>
2024-05-14 11:12:01 -0400

    white space

--- CremonaTrick.m
+++ CremonaTrick.m
@@ -21,11 +21,11 @@ function NumberFieldGCD(L)
 end function;
 
 intrinsic CremonaTrick(rec)->.
-{ Cremona's trick. }
+	{ Cremona's trick. }
 	if #rec lt 2 then
 		return rec[1][1];
 	end if;
-	prec := Min(&cat[ [Precision(x) : x in L[1] ] : L in rec]); 
+	prec := Min(&cat[ [Precision(x) : x in L[1] ] : L in rec]);
 	K1 := BaseField(rec[1][2][1]`name[2]);
 	K<a> := NumberFieldExtra(ChangeRing(DefiningPolynomial(K1), RationalsExtra(prec)));
 	c := [ [ y where _,y := AlgebraizeElementExtra(L[1][i] / rec[1][1][i], K) : L in rec[[1..#rec]] ] : i in [1,2] ];
@@ -34,25 +34,25 @@ intrinsic CremonaTrick(rec)->.
 end intrinsic;
 
 function MatchRoots(H, poly, qs)
-    prec := Precision(Universe(qs));
-    err := 10^(-prec/2);
+	prec := Precision(Universe(qs));
+	err := 10^(-prec/2);
 	//poly_facts := Factorisation(poly);
-	//assert( Min([Abs(q1-q2) : q1,q2 in qs | q1 ne q2]) gt 2*err ); 
-    vs := InfinitePlaces(H);
-    rs := [el[1] : el in Roots(poly, H)];
-    rs_sorted := [];
-    for r in rs do
-        for i->q in qs do
-            if Abs(Evaluate(r, vs[i]) - q) gt err then
+	//assert( Min([Abs(q1-q2) : q1,q2 in qs | q1 ne q2]) gt 2*err );
+	vs := InfinitePlaces(H);
+	rs := [el[1] : el in Roots(poly, H)];
+	rs_sorted := [];
+	for r in rs do
+		for i->q in qs do
+			if Abs(Evaluate(r, vs[i]) - q) gt err then
 				continue r;
 			end if;
-        end for;
+		end for;
 		return r;
-    end for;
+	end for;
 end function;
 
-intrinsic CremonaTrickWithEmbeddings(H, res_i : dim := 4)->.
-{ Cremona's trick with embeddings. }
+intrinsic CremonaTrickWithEmbeddings(H::Any, res_i::Any : dim := 4) -> Any
+	{ Cremona's trick with embeddings. }
 	assert assigned H`CC;
 	if #res_i lt 2 then
 		return [ res_i[1][k][1] : k in [1..dim] ];
@@ -89,6 +89,6 @@ intrinsic CremonaTrickWithEmbeddings(H, res_i : dim := 4)->.
 		Append(~L, MatchRoots(H, poly_QQ, qs));
 	end for;
 	//print L, [Norm(x) : x in L];
-    gcd := NumberFieldGCD(L);
-    return [ res_i[1][k][1] * Evaluate(gcd, InfinitePlaces(H)[k]) : k in [1..dim]], L;
+	gcd := NumberFieldGCD(L);
+	return [ res_i[1][k][1] * Evaluate(gcd, InfinitePlaces(H)[k]) : k in [1..dim]], L;
 end intrinsic;
