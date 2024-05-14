981a1cd90b99924828cb78963cef68d7bdfb0bb6
Edgar Costa <edgarc@mit.edu>
2024-05-14 12:16:33 -0400

    LMFDBHMFwithEigenvalues

--- Eigenvalues.m
+++ Eigenvalues.m
@@ -1,52 +1,73 @@
-intrinsic WriteEigenvalues(f, filename : Overwrite := false) -> RngIntElt
+intrinsic WriteEigenvalues(f, filename : Overwrite:=false) -> RngIntElt
 { Write eigenvalues to a file. }
-	if Overwrite then
-    	System("rm " cat filename);
-	end if;
-    F := NumberField(Ring(Universe(Keys(f`hecke_eigenvalues))));
-    keys := [k : k in Keys(f`hecke_eigenvalues)];
-    labels := [[StringToInteger(elt) : elt in Split(LMFDBLabel(k), ".")] : k in keys];
-    ParallelSort(~labels, ~keys);
-    for i->k in keys do
-        label := Join([Sprint(elt) : elt in labels[i]], ".");
-        eigenval := Sprint(Eltseq(f`hecke_eigenvalues[k]));
-        Write(filename, Join([label, eigenval], ":"));
-    end for;
-    return #keys + 1;
+  F := NumberField(Ring(Universe(Keys(f`hecke_eigenvalues))));
+  keys := [k : k in Keys(f`hecke_eigenvalues)];
+  FastFormat := #filename ge 5 and filename[#filename-4..#filename] eq ".fast";
+  labels := [[StringToInteger(elt) : elt in Split(LMFDBLabel(k), ".")] : k in keys];
+  ParallelSort(~labels, ~keys);
+  output := [];
+
+  for i->k in keys do
+    if FastFormat then
+      ideal := Sprint([Eltseq(g) : g in Generators(k)]);
+    else
+      // recompute label from sorting
+      ideal := Join([Sprint(elt) : elt in labels[i]], ".");
+    end if;
+    eigenval := Sprint(Eltseq(f`hecke_eigenvalues[k]));
+    Append(~output, Join([label, eigenval], ":"));
+  end for;
+  Write(filename, Join(output, "\n") : Overwrite:=Overwrite);
+  return #output;
 end intrinsic;
 
-intrinsic LoadEigenvalues(~f, filename : hasheader := false)
+intrinsic LoadEigenvalues(~f, filename)
 { Load the eigenvalues from a file. }
-	F<w> := BaseRing(Parent(f));
-	OF := Integers(F);
-	K := HeckeEigenvalueField(Parent(f));
-	coeffs := getrecs(filename);
 
-    range := [1..#coeffs];
-
-	for i in range do
-		rec := coeffs[i];
-        if "." in rec[1] then
-            ideal := LMFDBIdeal(OF, rec[1]);
-        else
-            ideal := ideal<OF | [OF!g : g in eval rec[1]]>;
-        end if;
-        if IsZero(ideal) then continue; end if;
-        try
-		    f`hecke_eigenvalues[ideal] := K!eval rec[2];
-        catch e
-            print "Could not process", rec;
-        end try;
-	end for;
+  F<w> := BaseRing(Parent(f));
+  OF := Integers(F);
+  K := HeckeEigenvalueField(Parent(f));
+  coeffs := getrecs(filename);
+  for i in [1..#coeffs] do
+    rec := coeffs[i];
+    if "." in rec[1] then
+      ideal := LMFDBIdeal(OF, rec[1]);
+    else
+      ideal := ideal<OF | [OF!g : g in eval rec[1]]>;
+    end if;
+    if IsZero(ideal) then continue; end if;
+    try
+      f`hecke_eigenvalues[ideal] := K!eval rec[2];
+    catch e
+      print "Could not process", rec;
+    end try;
+  end for;
 end intrinsic;
 
-intrinsic ConvertEigenvalues(label, path)
-{ }
-    f := LMFDBHMF(label);
-    LoadEigenvalues(~f, path cat label cat ".txt");
-    Write(path cat label cat ".txt.fast",
-        Join(
-            [StripWhiteSpace(Join([Sprint([Eltseq(g) : g in Generators(k)]),Sprint(Eltseq(v))], ":"))
-            :
-            k->v in f`hecke_eigenvalues],"\n") cat "\n" : Overwrite:=true);
+intrinsic LMFDBHMFwithEigenvalues(label, path : Convert:=true) -> ModFrmHilElt
+  {Create an eigenform from a label and loads its precomputed eigenvalues}
+  f := LMFDBHMF(label);
+  filename := path cat label cat ".txt";
+  filenamefast := filename cat ".fast"
+
+  // let's figure out if there is anything to do
+  bslow, slow := OpenTest(filename, "r");
+  require bslow: Sprintf("Cannot open %o", filename);
+
+  // file doesn't exist or the number of lines do not match
+  bfast, fast := OpenTest(filenamefast, "r");
+  if not bfast or #Split(Read(slow), "\n") ne #Split(Read(fast), "\n") then
+    LoadEigenvalues(~f, filename);
+  else
+    LoadEigenvalues(~f, filenamefast);
+    Convert := false;
+  end if;
+  delete slow;
+  delete fast;
+  if Convert then
+    WriteEigenvalues(f, fielnamefast);
+  end if;
+  return f;
 end intrinsic;
+
+
