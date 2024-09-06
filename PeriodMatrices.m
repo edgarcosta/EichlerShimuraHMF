b49d5b86eabea90a7de9dc3b669516acfb215c98
Edgar Costa <edgarc@mit.edu>
2024-09-06 19:57:11 +0100

    implementing Goren PeriodMatrix and Pairing

--- PeriodMatrices.m
+++ PeriodMatrices.m
@@ -1,4 +1,7 @@
 freeze;
+
+
+
 /*
 Old functions that resulted in pairing in the inverse different or something instead of ZZ
 
@@ -25,6 +28,8 @@ intrinsic ModuliToSmallPeriodMatrix(F, points) -> AlgMatElt
 end intrinsic;
 */
 
+
+/* We decided to go for the Goren standard
 // Given a totally real field H of degree g and a g-tuple of points in the upper half-plane, return the corresponding big period matrix
 intrinsic ModuliToBigPeriodMatrixNoam(H, points : fix_signs := false) -> AlgMatElt
 { Modified version of the ModiliToBigPeriodMatrix. }
@@ -47,6 +52,50 @@ intrinsic ModuliToSmallPeriodMatrixNoam(F, points) -> AlgMatElt
 { Given a list of taus and a totally real field, compute a small period matrix. }
     return SmallPeriodMatrix(ModuliToBigPeriodMatrixNoam(F, points));
 end intrinsic;
+*/
 
 
-// TODO: PeriodMatrixToModuli
+intrinsic Polarization(A::RngOrdFracIdl, B::RngOrdFracIdl) -> AlgMatElt
+{ given two fractional ideals, returns the matrix representing the pairing E_r on A+B defined as
+  E_r((x1, y1), (x2, y2)) = Trace(r*(x1*y2 - x2*y1)) }
+  // we expect A and B to be two fractional ideals
+  O := Order(A);
+  require O eq Order(B) : "A and B must be ideals of the same order";
+  D := Different(O);
+  b, r := IsNarrowlyPrincipal((D*A*B)^-1);
+  require b : "A and B must be so that A*B*Different is narrowly principal";
+  // Alternatively
+  // E := Matrix([[Trace(r*(x1*y2-x2*y1)) where x2, y2 := Explode(b2) : b2 in B] where x1, y1 := Explode(b1): b1 in B]) where B := [<x,0> : x in Basis(A)] cat [<0,y> : y in Basis(B)];
+  P := Matrix(Integers(), [[Trace(r*x*y) : x in Basis(B)] : y in Basis(A)]);
+  E := BlockMatrix([[0, P], [-Transpose(P), 0]]);
+  return E;
+end intrinsic;
+
+intrinsic ModuliToPeriodMatrix(z::FldComElt[], A::RngOrdFracIdl, B::RngOrdFracIdl) -> ModMatFldElt
+{ Returns the period matrix asociated to the lattice A*z + B*(1,...,1) }
+  prec := Min([Precision(Parent(elt)) : elt in z]);
+  CC := ComplexFieldExtra(prec);
+  O := Order(A);
+  K := NumberField(O);
+  require &and[ Im(elt) gt 0 : elt in z]: "We expect the entries of z have positive imaginary part";
+  require Degree(K) eq #z: "The degree of numberfield K must match the number of elements in the upper half-plane";
+  PA := Matrix([[CC | Evaluate(a, pl : Precision := prec + 10)*z[i] : a in Basis(A) ] : i->pl in InfinitePlaces(K)]);
+  PB := Matrix([[CC | Evaluate(b, pl : Precision := prec + 10) : i->b in Basis(B)] : pl in InfinitePlaces(K)]);
+  return HorizontalJoin([PA, PB]);
+end intrinsic;
+
+intrinsic ModuliToBigPeriodMatrix(z, A, B)
+{ Returns the period matrix asociated to the lattice A*z + B*(1,...,1), in basis, such that it is equipped with the standard symplectic pairing }
+  P := ModuliToPeriodMatrix(z, A, B);
+  E := Polarization(A, B);
+  g := Nrows(E) div 2;
+  S, F := FrobeniusFormAlternating(E);
+  assert S eq StandardSymplecticMatrix(g);
+  bigP := P*Transpose(ChangeRing(F, BaseRing(P)));
+  return bigP;
+end intrinsic;
+
+intrinsic ModuliToSmallPeriodMatrix(z, A, B)
+{ Returns the small period matrix asociated to the lattice A*z + B*(1,...,1) }
+  return SmallPeriodMatrix(ModuliToBigPeriodMatrix(z, A, B));
+end intrinsic;
